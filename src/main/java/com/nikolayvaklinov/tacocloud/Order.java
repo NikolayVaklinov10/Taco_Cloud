a84a34af0c006f031c29791f822717157105914a
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-06-29 13:14:39 +0100

    fields added for persistence

--- src/main/java/com/nikolayvaklinov/tacocloud/Order.java
+++ src/main/java/com/nikolayvaklinov/tacocloud/Order.java
@@ -8,8 +8,15 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Order {
+
+    private Long id;
+
+    private Date placedAt;
+
 
     //end::allButValidation[]
     @NotBlank(message="Name is required")
