11faf2908afd585430c3a28bf55392da2b1d2452
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-07-01 15:04:40 +0100

    for configuration

--- src/main/java/com/nikolayvaklinov/tacocloud/data/OrderRepository.java
+++ src/main/java/com/nikolayvaklinov/tacocloud/data/OrderRepository.java
@@ -1,9 +1,20 @@
 package com.nikolayvaklinov.tacocloud.data;
 
 import com.nikolayvaklinov.tacocloud.Order;
+import com.nikolayvaklinov.tacocloud.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface OrderRepository
         extends CrudRepository<Order, Long> {
+
+    List<Order> findByUserOrderByPlacedAtDesc(
+            User user, Pageable pageable);
+
+  /*
+  List<Order> findByUserOrderByPlacedAtDesc(User user);
+   */
 
 }
