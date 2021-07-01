577406a4eb7f080d768dd5ac5b3a15ca13720165
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-07-01 14:06:17 +0100

    the order controller

---
+++ src/main/java/com/nikolayvaklinov/tacocloud/web/OrderController.java
@@ -0,0 +1,69 @@
+package com.nikolayvaklinov.tacocloud.web;
+import javax.validation.Valid;
+
+import com.nikolayvaklinov.tacocloud.User;
+import com.nikolayvaklinov.tacocloud.data.OrderRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+//end::baseClass[]
+//tag::baseClass[]
+
+import lombok.extern.slf4j.Slf4j;
+import com.nikolayvaklinov.tacocloud.Order;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("order")
+public class OrderController {
+
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    @GetMapping("/current")
+    public String orderForm(@AuthenticationPrincipal User user,
+                            @ModelAttribute Order order) {
+        if (order.getDeliveryName() == null) {
+            order.setDeliveryName(user.getFullname());
+        }
+        if (order.getDeliveryStreet() == null) {
+            order.setDeliveryStreet(user.getStreet());
+        }
+        if (order.getDeliveryCity() == null) {
+            order.setDeliveryCity(user.getCity());
+        }
+        if (order.getDeliveryState() == null) {
+            order.setDeliveryState(user.getState());
+        }
+        if (order.getDeliveryZip() == null) {
+            order.setDeliveryZip(user.getZip());
+        }
+
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
+
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        order.setUser(user);
+
+        orderRepo.save(order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+
+}
+
