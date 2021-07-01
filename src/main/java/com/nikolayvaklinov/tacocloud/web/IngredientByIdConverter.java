8c1c74bba4ea4baaa65aa60e568eb6be0834f0db
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-07-01 14:08:06 +0100

    the ingredient by id converter

---
+++ src/main/java/com/nikolayvaklinov/tacocloud/web/IngredientByIdConverter.java
@@ -0,0 +1,28 @@
+package com.nikolayvaklinov.tacocloud.web;
+
+import com.nikolayvaklinov.tacocloud.Ingredient;
+import com.nikolayvaklinov.tacocloud.data.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private IngredientRepository ingredientRepo;
+
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        Optional<Ingredient> optionalIngredient = ingredientRepo.findById(id);
+        return optionalIngredient.isPresent() ?
+                optionalIngredient.get() : null;
+    }
+
+}
